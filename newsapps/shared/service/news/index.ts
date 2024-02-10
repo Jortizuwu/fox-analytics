838314063e03ffef5471f3416bc737ace93bcc7b
Jortizuwu <joseher2511@gmail.com>
2024-02-10 13:36:58 -0500

    med

--- newsapps/shared/service/news/index.ts
+++ newsapps/shared/service/news/index.ts
@@ -1,13 +1,10 @@
 import { api } from '@/shared/common/api'
 import { IsigninResponse } from '@/shared/types/interface'
-import { getEnv } from '@/shared/utils/get-env'
-
-const API_KEY = getEnv('EXPO_PUBLIC_API_KEY')
 
 export const newServices = {
   listNews: async ({ pageParam }: { pageParam: number }) => {
     const { data } = await api.get<IsigninResponse>(
-      `/everything?q=a&pageSize=20&page=${pageParam}&apiKey=${API_KEY}`,
+      `/everything?q=a&pageSize=20&page=${pageParam}&apiKey=4931cae34d7a4d24b208d55ce8bcab66`,
     )
 
     return data
